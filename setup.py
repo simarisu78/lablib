e0c3b193aa821dfa15d443373c811bf713df8170
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2021-08-18 04:38:48 +0900

    Add configure file for module Test
    add setup.py and setup.cfg to root directory.
    On branch master
     Changes to be committed:
    	new file:   setup.cfg
    	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from glob import glob
+from os.path import basename
+from os.path import splitext
+
+from setuptools import setup,find_packages
+
+setup(
+packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    zip_safe=False,
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov"],
+)
