c7fb0f9b6e225933511c788cbbf01a7d2351008c
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2021-08-20 11:31:32 +0900

    style: modify code style with black

--- setup.py
+++ setup.py
@@ -4,35 +4,39 @@ from os.path import basename
 from os.path import splitext
 import subprocess
 
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
+
 
 class VirtualEnvTest(TestCommand):
 
-    description = 'run unit test in virtual environment (e.g. barcode reader is not connected)'
+    description = (
+        "run unit test in virtual environment (e.g. barcode reader is not connected)"
+    )
     user_options = []
 
     def run_tests(self):
-        self._run(['pytest', '-c', 'pytest_env.ini'])
-        
+        self._run(["pytest", "-c", "pytest_env.ini"])
+
     def _run(self, command):
         try:
             subprocess.check_call(command)
         except Exception as e:
             pass
 
+
 def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
-        
+
 setup(
-packages=find_packages("src"),
+    packages=find_packages("src"),
     package_dir={"": "src"},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     setup_requires=["pytest-runner"],
     tests_require=["pytest", "pytest-cov"],
-    cmdclass={'envtest':VirtualEnvTest},
-    install_requires=_requires_from_file('requirements.txt'),
+    cmdclass={"envtest": VirtualEnvTest},
+    install_requires=_requires_from_file("requirements.txt"),
 )
