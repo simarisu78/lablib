4095fb82be5d8276bc7f6b6085eb84c5c2fa3a5b
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-01-30 05:48:41 +0900

    Fix app.py
    Move the execution script to run.py

--- src/lablib/app/app.py
+++ src/lablib/app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, send_from_directory
 from markupsafe import escape
 import os
 
-from api import api
+from app.api import api
 
 
 app = Flask(__name__, static_folder="static")
@@ -30,13 +30,3 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/media'),
                                'favicon.ico', )
 
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='Hoge Huga'))
-
-
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
