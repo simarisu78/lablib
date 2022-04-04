4f732b987667e645e362ae4e5068c765bc863ac3
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-04-05 05:15:52 +0900

    Add: magazine registration function

--- lablib/app/api.py
+++ lablib/app/api.py
@@ -51,16 +51,21 @@ def register_books():
 		return jsonify({"status":"ng", "msg":"can not read book data. Please sent book data in json's list format"})
 
 	if selfRegister:
+		ngList = []
 		try:
 			for book_req in books:
 				barcode = book_req.get('barcode')
+    
+				if Book.query.filter_by(barcode=barcode) is not None:
+					ngList.append({"barcode":barcode, "msg":"this book is already registered"})
+					continue
+    
 				title = book_req.get('title')
 				author = book_req.get('author')
 				detail = book_req.get('detail')
 				publishmonth = book_req.get('publishmonth')
 				publisher = book_req.get('publisher')
-				amount = 1
-				stock = amount
+				amount = stock = 1
 
 				barcode_pat = re.compile("\d+")
 				publishmonth_pat = re.compile("\d{4}-\d{2}")
@@ -74,11 +79,14 @@ def register_books():
 
 			db.session.commit()
 		except ValueError:
-			return jsonify({"status":"ng", "msg":"The format is invalid. Please check barcode and publishmonth."})
+			return ngList.append({"status":"ng", "msg":"The format is invalid. Please check barcode and publishmonth."})
 		except:
-			return jsonify({"status":"ng", "msg":"Some error occured. Please check your book data. barcode, title, author, publishmonth are required."})
+			return ngList.append({"status":"ng", "msg":"Some error occured. Please check your book data. barcode, title, author, publishmonth are required."})
 
-		return jsonify({"status": "ok"})
+		if len(ngList) == 0:
+			return jsonify({"status": "ok"})
+		else:
+			return jsonify({"status": "partial ng", "ngList":ngList})
 	else:
 		return search_external_api(books)
 
@@ -86,29 +94,89 @@ def register_books():
 # default api is OpenBD
 import requests
 import time
+RAKUTEN_ICHIBA = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
+RAKUTEN_BOOKS = "https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404"
+OPENBD = "https://api.openbd.jp/v1/get"
+MAGAZINE_CODELIST = {"05827":"Software Design",
+                     "01619":"Interface"}
+AUTHOR_LIST = {"05827":"技術評論社",
+               "01619":"ＣＱ出版"}
+TRANSLATE_LIST = {
+  '\u3000': ' ',
+}
 def search_external_api(books):
 	notFoundList = []
 	for books_req in books:
 		try:
 			barcode = books_req.get("barcode")
-			data = {"applicationId": current_app.config['RAKUTEN_APPLICATION_ID'],
-				"format":"json",
-				"isbnjan": str(barcode)}
-			req = requests.get("https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404", data)
-
-			if req.status_code != 200:
-				raise ConnectionRefusedError
+			if barcode is None:
+				notFoundList.append({"msg":"Please pass barcodes"})
+				continue
+			
+			if Book.query.filter_by(barcode=barcode) is not None:
+				notFoundList.append({"barcode":barcode, "msg":"this book is already registered"})
+				continue
    
-			if req.json()['hits'] == 0:
-				raise NotFoundErr
-			result = req.json()['Items'][0]['Item']
+			newbook = Book(barcode=barcode)
+			
+			# 雑誌の場合
+			# 楽天市場APIを検索
+			# 雑誌コードを事前に登録しておき、年月だけ抽出する
+			if barcode[:3] == '491':
+       
+				data = {"applicationId": current_app.config['RAKUTEN_APPLICATION_ID'],
+						"format":"json",
+						"keyword":str(barcode)}
+				res = requests.get(RAKUTEN_ICHIBA, data)
+				if res.status_code != 200:
+					raise ConnectionRefusedError
+		
+				if res.json().get('hits') == 0:
+					raise NotFoundErr
 
-			publishmonth = '-'.join(re.search("(\d+)年(\d+)月", result['salesDate']).groups())
-			newbook = Book(barcode=barcode, title=result['title'], 
-				author=result['author'], publishmonth=publishmonth,
-				publisher=result['publisherName'], amount=1, stock=1,
-				large_url=result['largeImageUrl'])
-   
+				year_month = ""
+				publish_month = ""
+				for item in res.json().get("Items"):
+					tmp = item.get("Item").get("itemName")
+					reg = re.search("(\d+).*年.*(\d+).*月", tmp)
+					if reg is not None:
+						year_month = "{}年{:0>2}月号".format(reg.groups()[0], reg.groups()[1])
+						publish_month = "-".join([reg.groups()[0], "{:0>2}".format(reg.groups()[1]) ])
+						break
+				
+				magazine_code = barcode[4:9]
+				if magazine_code in MAGAZINE_CODELIST:
+					newbook.title = " ".join([MAGAZINE_CODELIST.get(magazine_code), year_month])
+					newbook.publishmonth = publish_month
+					newbook.author = newbook.publisher = AUTHOR_LIST.get(magazine_code)
+					newbook.amount = newbook.stock = 1
+     
+				else:
+					notFoundList.append({"barcode":barcode, "msg":"Not registered in the magazine list.Please tell the administrator."})
+					continue
+			
+			# JAN以外の場合（ほぼISBN）
+			else:
+				data = {"isbn":barcode, "pretty":True}
+				res = requests.get(OPENBD, data)
+
+				if res.status_code != 200:
+					raise ConnectionRefusedError
+	
+				if res.json()[0] is None:
+					raise NotFoundErr
+				result = res.json()[0].get("onix")
+
+				newbook.title = result.get("DescriptiveDetail").get("TitleDetail").get("TitleElement").get		("TitleText").get("content")
+				newbook.author = result.get("DescriptiveDetail").get("Contributor")[0].get("PersonName").get("content").translate(TRANSLATE_LIST)
+				newbook.detail = result.get("CollateralDetail", {}).get("TextContent", {})[0].get("Text", None)
+
+				pubdate = result.get("PublishingDetail").get("PublishingDate")[0].get("Date")
+				newbook.publishmonth = "-".join([pubdate[:4], pubdate[4:6]])
+				newbook.publisher = result.get("PublishingDetail", {}).get("Publisher", {}).get("PublisherName", None)
+				newbook.amount = newbook.stock = 1
+				newbook.large_url = result.get("CollateralDetail", {}).get("SupportingResource", {})[0].get("ResourceVersion", {})[0].get("ResourceLink", None)
+    
 			db.session.add(newbook)
 			db.session.commit()
    
@@ -118,8 +186,12 @@ def search_external_api(books):
 		except ConnectionRefusedError:
 			return jsonify({"status":"ng", "msg":"Connection to API was Refused. Please check token."})
 
-		except:
+		except KeyError:
+			notFoundList.append({"barcode":barcode,"msg":"Required information was not available. Please register manually."})
+		
+		except Exception:
 			notFoundList.append({"barcode":barcode,"msg":"some error occured."})
+
 		finally:
 			db.session.rollback()
 			db.session.close()
