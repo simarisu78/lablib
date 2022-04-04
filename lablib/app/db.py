01a30ecb00d7c6794ffec09464235e6fa9a05689
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-04-05 05:13:05 +0900

    Fix: Initialize database on startup

--- lablib/app/db.py
+++ lablib/app/db.py
@@ -5,11 +5,15 @@ from sqlalchemy import event
 
 db = SQLAlchemy()
 
+"""
 @event.listens_for(sqlalchemy.engine.Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys = ON")
     cursor.close()
+"""
 
 def init_db(app):
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
