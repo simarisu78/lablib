3fb857ae1a12099193e1138d10cd15e8fcf18f44
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-04-05 09:13:08 +0900

    Add: sample configuration

--- lablib/app/config.py
+++
@@ -1,7 +0,0 @@
-# Development config
-# DO NOT USE PRODUCTION SERVER
-class DevelopmentConfig():
-      DEBUG = True
-      SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
-
-Config = DevelopmentConfig
---
+++ lablib/app/config_sample.py
@@ -0,0 +1,24 @@
+# When you use this application, rename this file to config.py
+from datetime import timedelta
+
+class LablibConfig():
+	#SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+	SQLALCHEMY_DATABASE_URI = '[URI of your database server]'
+	JSON_SORT_KEYS = False
+	JSON_AS_ASCII = False
+	
+	# Flask JWT
+	JWT_SECRET_KEY = '[your secret key]'
+	JWT_ALGORITHM = 'HS256'		# Sigunature Algorithm
+	JWT_LEEWAY = 0
+	JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+	JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+	JWT_TOKEN_LOCATION = ['headers', 'json']
+ 
+	# LDAP
+	BASE_DOMAIN = '[domain (e.g. cn=Users,dc=dc,dc=localdomain)]'
+	LDAP_SERVER = '[ip or FQDN of your ldap server]'
+ 
+	RAKUTEN_APPLICATION_ID = '[Your application ID here!]'
+ 
+Config = LablibConfig
