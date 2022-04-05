88e2ab9bd6d41917aa106b3f80396fda935ba7bb
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-04-06 05:46:42 +0900

    Fix: changed ldap username and password

--- test/test_flask/test_books.py
+++ test/test_flask/test_books.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+import os
 from logging import getLogger
 
 logger = getLogger(__name__)
@@ -10,8 +11,9 @@ class Testbooks:
 
 	@pytest.mark.ssnlib
 	def test_register_books(self, client):
-		auth_res = client.post("/api/v1/auth", data=json.dumps(dict(username="testuser", password="Ohghee2eeyoo0ael")), headers={"content-type":"application/json"})
+		auth_res = client.post("/api/v1/auth", data=json.dumps(dict(username="Administrator", password=os.environ.get("LDAP_ADMIN_PASSWD"))), headers={"content-type":"application/json"})
 		JWT = auth_res.json.get("access_token")
+		logger.debug("access_token is: {}".format(JWT))
 
 		res = client.post("/api/v1/books", headers={"Authorization":"Bearer {}".format(JWT), "content-type":"application/json"}, data=json.dumps({"books":[{"barcode":BARCODES[0]}]}))
 		logger.info(res.json)
