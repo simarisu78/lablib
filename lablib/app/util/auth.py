0d50245a646f925448219d2df6985ce9c00b3762
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-04-12 16:44:02 +0900

    Fix: Changed API access intervals to be managed on the server side

--- lablib/app/util/auth.py
+++ lablib/app/util/auth.py
@@ -1,10 +1,10 @@
-#from flask import current_app
+from flask import current_app
 from ldap3 import Connection
-from lablib.app import app
+#from lablib.app import app
 
 def ldap_auth(username, password):
-	BASE_DOMAIN = app.config.get("BASE_DOMAIN")
-	LDAP_SERVER = app.config.get("LDAP_SERVER")
+	BASE_DOMAIN = current_app.config.get("BASE_DOMAIN")
+	LDAP_SERVER = current_app.config.get("LDAP_SERVER")
 
 	domain = ','.join(['cn={}'.format(username), BASE_DOMAIN])
 	conn = Connection(LDAP_SERVER, domain, password=password)
