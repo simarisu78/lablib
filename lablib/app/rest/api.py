bfa44bafed75669446257db6b45e784b2cb57760
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-04-17 17:09:19 +0900

    Add: add api for checkout books

--- lablib/app/rest/api.py
+++ lablib/app/rest/api.py
@@ -4,9 +4,10 @@ from flask_jwt_extended import jwt_required, create_access_token
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 from lablib.app.util.auth import ldap_auth
-from lablib.app.models import Book, BookSchema, Checkout
+from lablib.app.models import Book, BookSchema, Checkout, Users
 from lablib.app import limiter
 from .book_register import self_register, search_external_api
+from lablib.app.util.db import db
 
 # content_type checker
 def content_type(value):
@@ -21,6 +22,12 @@ def content_type(value):
 			return func(*args, **kwargs)
 		return wrapper
 	return _content_type
+
+def make_ng_res(msg):
+	return jsonify({"status":"ng", "msg":msg})
+
+def make_ok_res():
+	return jsonify({"status":"ok"})
 
 # generate token
 @api.route('/auth', methods=['POST'])
@@ -61,10 +68,37 @@ def register_books():
 		return search_external_api(books)
 
 # borrow books
+from logging import getLogger
+logger = getLogger(__name__)
 @api.route('/checkout', methods=['POST'])
 @content_type("application/json")
 def borrow_books():
-	return "check out books"
+	student_id = request.json.get("student_id")
+	barcode = request.json.get("barcode")
+	logger.info(student_id)
+	logger.info(barcode)
+	if student_id == None or barcode == None:
+		return make_ng_res("please post student_id and barcode in json format")
+	
+	try:
+		user_id = Users.query.filter_by(student_id=student_id).first()
+		book_id = Book.query.filter_by(barcode=barcode).first()
+
+		if user_id is None:
+			return make_ng_res("this user is unregistered")
+		if book_id is None:
+			return make_ng_res("this book does not exist")
+
+		checkout = Checkout(user_id=user_id, book_id=book_id)
+
+		db.session.add(checkout)
+		db.session.commit()
+
+		return make_ok_res()
+	except:
+		db.session.rollback()
+		db.session.close()
+
 
 # return books
 @api.route('/checkout', methods=['DELETE'])
