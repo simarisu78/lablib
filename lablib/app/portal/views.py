731f130c41a97a82a0122b41413efb3f24afcd77
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-05-03 22:25:16 +0900

    Add: toppage - show book list

--- lablib/app/portal/views.py
+++ lablib/app/portal/views.py
@@ -1,21 +1,28 @@
-from flask import render_template, send_from_directory
+from asyncio.log import logger
+from flask import render_template, send_from_directory, request
+from flask_paginate import Pagination, get_page_parameter
 from markupsafe import escape
 
 from lablib.app import app
+from lablib.app.models import Book
 
 @app.route('/')
 def index():
-    return render_template('layout.html')
+	books = Book.query.all()
+	page = request.args.get(get_page_parameter(), type=int, default=1)
+	rows = books[(page-1)*30 : page*30]
+	pagination = Pagination(page=page, total=len(books), per_page=30, css_framework='bootstrap5')
+	return render_template('toppage.html', pagination=pagination, rows=rows)
 
 @app.route('/login')
 def login():
-    return 'login'
+	return 'login'
 
 @app.route('/users/<username>')
 def profile(username):
-    return '{}\'s profile'.format(escape(username))
+	return '{}\'s profile'.format(escape(username))
 
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory('static/media', 'favicon.ico')
+	return send_from_directory('static/media', 'favicon.ico')
