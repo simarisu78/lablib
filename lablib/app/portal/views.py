933abca5969c62c36991468c4d3ac5050e9ff9d8
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-05-04 08:03:15 +0900

    Add: book search functions

--- lablib/app/portal/views.py
+++ lablib/app/portal/views.py
@@ -1,4 +1,6 @@
+from ast import keyword
 from asyncio.log import logger
+from crypt import methods
 from flask import render_template, send_from_directory, request
 from flask_paginate import Pagination, get_page_parameter
 from markupsafe import escape
@@ -6,9 +8,14 @@ from markupsafe import escape
 from lablib.app import app
 from lablib.app.models import Book
 
-@app.route('/')
+@app.route('/', methods=['get', 'post'])
 def index():
-	books = Book.query.all()
+	keyword = request.form.get('keyword')
+	if keyword is None:
+		books = Book.query.all()
+	else:
+		books = Book.query.filter(Book.title.like("%{}%".format(keyword))).all()
+
 	page = request.args.get(get_page_parameter(), type=int, default=1)
 	rows = books[(page-1)*30 : page*30]
 	pagination = Pagination(page=page, total=len(books), per_page=30, css_framework='bootstrap5')
