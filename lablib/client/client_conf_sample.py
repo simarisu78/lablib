4b8e9ff4a449be39d959af20baf1921185ed57a0
simarisu78 <61213707+simarisu78@users.noreply.github.com>
2022-05-05 17:23:46 +0900

    Add: book register function in guiclient

---
+++ lablib/client/client_conf_sample.py
@@ -0,0 +1,4 @@
+import os
+class CLIENT_CONF:
+    REGISTER_URL = "URL HERE!"
+    TOKEN = '<JWT_TOKEN HERE!>'
--- lablib/client/guiClient.py
+++ lablib/client/guiClient.py
@@ -2,22 +2,32 @@ import PySimpleGUI as sg
 from logging import getLogger
 from queue import Queue
 import threading
+import hashlib
+import requests
+import os
 
 from barcode import BarcodeReader
-from felicaReader import FelicaReader
+from felica import FelicaReader
+from client_conf import CLIENT_CONF
+
+REGISTER_URL = CLIENT_CONF.REGISTER_URL
+TOKEN = CLIENT_CONF.TOKEN
 
 logger = getLogger(__name__)
 sg.theme('Light Brown')
 x, y = sg.Window.get_screen_size()
 
 # worker function for receive messages from barcode and felica reader
-
-
 def worker(queue, window):
     while True:
         event = queue.get()
         logger.debug("event received %s".format(event))
-        window.write_event_value("-THREAD-", event)
+        if event[0] == "barcode":
+            window.write_event_value("-BARCODE-", event)
+        elif event[0] == "nfc-connect":
+            window.write_event_value("-FELICA_CON-", event)
+        elif event[0] == "nfc-release":
+            window.write_event_value("-FELICA_REL-", event)
 
 
 def rent(studentID):
@@ -29,10 +39,9 @@ def return_book(barcode):
 
 
 def login():
-    import hashlib
-
     # THIS IS A PASSWORD FOR TESTING #
-    password_hash = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918"
+    password_hash = "66206db9bd44faccedf3a0af4c2afd34fbedf13795b0f3fea089892a599efb57"
+    salt = "akljfsadkljiuhkjlhr"
 
     message = "書籍の登録にはパスワードが必要です"
     layout = [
@@ -59,8 +68,9 @@ def login():
 
         elif event in ["-login_button-", '\r']:
             input = values["-password-"]
-            if hashlib.sha256(input.encode()).hexdigest() == password_hash:
+            if hashlib.sha256((salt + input).encode()).hexdigest() == password_hash:
                 login_window["-notice-"].update("Welcome")
+                register()
                 break
             else:
                 login_window["-notice-"].update("Login Failed")
@@ -69,7 +79,75 @@ def login():
 
 
 def register():
-    pass
+    message = "登録したい書籍のバーコードを読み込んでください"
+    message2 = "エラーが発生した場合は直接入力してください"
+    layout = [
+        [sg.Text(message)],
+        [sg.Text(message2)],
+        [sg.Text("", key="-notice1-", text_color="Green")],
+        [sg.Text("", key="-notice2-", text_color="Red")],
+        [sg.Text("Barcode"), sg.Input("", key="-barcode-")],
+        [sg.Text("Title"), sg.Input("", key="-title-")],
+        [sg.Text("Author"), sg.Input("", key="-author-")],
+        [sg.Button("Apply", key="-apply_button-"),
+         sg.Button("Cancel", key="-cancel_button-")],
+    ]
+    register_window = sg.Window("Register Window",
+                             layout,
+                             return_keyboard_events=True,
+                             element_justification='center',
+                             keep_on_top=True,
+                             size=(x, y)).Finalize()
+    register_window.Maximize()
+
+    header = {"Authorization" : " ".join(["Bearer",TOKEN])}
+    while True:
+        window, event, values = sg.read_all_windows()
+
+        if event in [sg.WIN_CLOSED, '-exit-', 'Escape:27', "-cancel_button-"]:
+            break
+
+        if event in ["-BARCODE-"]:
+            barcode = values[1]
+            try:
+                res = requests.post(REGISTER_URL, json={"books":[{"barcode":barcode}]}, headers=header)
+            except:
+                register_window["-notice1-"].update("")
+                register_window["-notice2-"].update("Connection refused. Please check server")
+                register_window["-barcode-"].update(barcode)
+                continue
+
+            if res.status_code == 200 and res.json().get("status") == "ok":
+                register_window["-notice1-"].update("Success! : Barcode {}".format(barcode))
+                register_window["-notice2-"].update("")
+            else:
+                register_window["-notice1-"].update("")
+                register_window["-notice2-"].update("Register Failure : Barcode {}".format(barcode))
+                register_window["-barcode-"].update(barcode)
+
+        if event in ["-apply_button-"]:
+            barcode = values["-barcode-"]
+            title = values["-title-"]
+            author = values["-author-"]
+
+            try:
+                res = requests.post(REGISTER_URL, json={"self":True ,"books":[{"barcode":barcode, "title":title, "author":author}]}, headers=header)
+            except:
+                register_window["-notice1-"].update("")
+                register_window["-notice2-"].update("Connection refused. Please check server")
+                register_window["-barcode-"].update(barcode)
+                continue
+
+            if res.status_code == 200 and res.json().get("status") == "ok":
+                register_window["-notice1-"].update("Success! : Barcode {}".format(barcode))
+                register_window["-notice2-"].update("")
+                register_window["-barcode-"].update("")
+            else:
+                register_window["-notice1-"].update("")
+                register_window["-notice2-"].update("Register Failure! Please check inputs".format(barcode))
+                register_window["-barcode-"].update(barcode)
+
+    register_window.close()
 
 
 def main():
@@ -101,9 +179,8 @@ def main():
     fr.daemon = True
     fr.start()
 
-    # thread for polling event queuee
-    threading.Thread(target=worker, args=(
-        event_queue, window), daemon=True).start()
+    # thread for polling event queue
+    #threading.Thread(target=worker, args=(event_queue, window), daemon=True).start()
 
     # Event Loop
     while True:
